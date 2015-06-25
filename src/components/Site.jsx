b23e0ea9baed8d532bd8538b1fce3dd96b84eb9f
Mikael Berg <miberg@silabs.com>
2015-06-24 22:43:42 -0500

    feat(components): `Site` takes `cssFilenames` property, renders link elems

--- src/components/Site.jsx
+++ src/components/Site.jsx
@@ -7,11 +7,20 @@ export default class Site extends React.Component {
     return <Router path={this.props.path} />;
   }
 
+  renderCss() {
+    let cssFilenames = this.props.cssFilenames ? this.props.cssFilenames : [];
+
+    return cssFilenames.map((filename) => {
+      return <link href={filename} key={filename} rel="stylesheet" />;
+    });
+  }
+
   render() {
     return (
       <html>
         <head>
           <title>Site</title>
+          {this.renderCss()}
         </head>
         <body>
           <h1>Site</h1>
@@ -22,3 +31,8 @@ export default class Site extends React.Component {
     );
   }
 }
+
+Site.propTypes = {
+  cssFilenames: React.PropTypes.array,
+  path: React.PropTypes.string
+};
