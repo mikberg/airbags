3847c90d8e0dd8e73b8d9c0f00170613be62638d
Mikael Berg <miberg@silabs.com>
2015-08-09 22:40:50 -0500

    refactor(pkg): Refactor index

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-import { Provider } from 'react-redux';
+import client from './client';
 import tasks from './src/tasks';
-import createAirbagsStore from './src/createAirbagsStore';
-import * as components from './src/components';
 
-export default {
-  tasks,
-  createAirbagsStore,
-  Provider,
-  components
-};
+export default Object.assign({}, client, {
+  tasks
+});
