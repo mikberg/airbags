44b34e192f18d947dc3be239f400389edce935bc
Mikael Berg <miberg@silabs.com>
2015-08-15 14:36:16 -0500

    feat(api): Add mock promise based api

---
+++ src/api/index.js
@@ -0,0 +1,14 @@
+export default class AirbagsApi {
+  getPage(path) {
+    return new Promise((resolve) => {
+      resolve({
+        content: '*Hi*, I am **content**!',
+        path,
+        frontMatter: {
+          title: 'Colophon',
+          tags: ['About', 'Cool', 'Awesome']
+        }
+      });
+    });
+  }
+}
