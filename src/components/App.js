daf8aa67e265fd430249392cad26462414083d72
Mikael Berg <miberg@silabs.com>
2015-08-16 14:48:20 -0500

    feat(app): `App` auto-loads cache from `window.__CACHE__` if it can

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import Home from './Home';
 import Menu from './Menu';
+import api from '../api';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+
+    if (process.browser && window.__CACHE__) {
+      api.loadCache(window.__CACHE__);
+    }
+  }
+
   renderRoute() {
     if (this.props.children) {
       return this.props.children;
