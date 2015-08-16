8751591505120b4a12cb7bf729078d8eeab9d97d
Mikael Berg <miberg@silabs.com>
2015-08-16 11:22:56 -0500

    refactor(page): Page once again fetches real content ...

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import AirbagsApi from '../api';
 
 export default class Page extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      page: null
+    };
+  }
+
+  componentDidMount() {
+    AirbagsApi
+      .getPage('/pages/colophon.json')
+      .then((page) => {
+        this.setState({page});
+      });
+  }
+
+  renderWithData() {
+    return (
+      <article>
+        <h2>{this.state.page.frontMatter.title}</h2>
+        {this.state.page.contents}
+      </article>
+    );
+  }
+
+  renderLoading() {
+    return <div>Loading ...</div>;
+  }
+
   render() {
-    return <div>Yes, this is page</div>;
+    return (
+      <div className="Page">
+        {() => {
+          if (this.state.page) {
+            return this.renderWithData();
+          }
+
+          return this.renderLoading();
+        }()}
+      </div>
+    );
   }
 }
