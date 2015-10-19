a4cfb4eed0fb9f84f0e1cb9a0a2188579e31f8de
Mikael Berg <miberg@silabs.com>
2015-10-19 21:01:15 +0200

    feat(Page): `Page` should force fetch new data when receiving new props

--- docs/components/Page.js
+++ docs/components/Page.js
@@ -4,6 +4,14 @@ import Transmit from 'react-transmit';
 export default class Page extends React.Component {
   static propTypes = {
     pageData: React.PropTypes.object.isRequired,
+    transmit: React.PropTypes.object.isRequired,
+    variables: React.PropTypes.object.isRequired,
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.variables.pageName !== this.props.variables.pageName) {
+      this.props.transmit.forceFetch(this.props.variables);
+    }
   }
 
   render() {
