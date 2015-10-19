72d674e9858e77c59459128e7618914144e09370
Mikael Berg <miberg@silabs.com>
2015-10-20 00:10:38 +0200

    feat(api): Api is now created through factory, `createApi(context, strategies)`
    BREAKING CHANGE: Api can no longer be created through `new AirbagsApi`, but has
    to be created via `createApi(context[, strategies])` instead.

--- src/api/index.js
+++ src/api/index.js
@@ -1,64 +1,58 @@
 import {isContextOk} from '../context';
 
+/**
+ * Run a method on strategies in order until one of them resolves. Reject if
+ * no strategies resolve.
+ */
+export function applyToStrategies(strategies, methodName, args) {
+  return new Promise((resolve, reject) => {
+    if (strategies.length === 0) {
+      return reject(new Error(`No strategies to use for ${methodName}`));
+    }
+
+    const apply = (iterator) => {
+      if (iterator === strategies.length) {
+        return reject(`No strategies could resolve ${methodName} for arguments ${args}`);
+      }
+
+      strategies[iterator][methodName](...args)
+        .then((data) => {
+          return resolve(data);
+        }).catch(() => {
+          apply(iterator + 1);
+        });
+    };
+
+    apply(0);
+  });
+}
+
+function apiModel(context, strategies) {
+  this.getPageData = (nakedPath) => {
+    return applyToStrategies(strategies, 'getPageData', [context, nakedPath]);
+  };
+
+  this.getPageHtml = (nakedPath) => {
+    /* eslint no-console:0 */
+    console.warn(`api.getPageHtml is deprecated, please use getPageData instead`);
+    return applyToStrategies(strategies, 'getPageHtml', [context, nakedPath]);
+  };
+}
+
 export function isStrategyOk(strategy) {
   return typeof strategy === 'object';
 }
 
-export default class AirbagsApi {
-  constructor(context, strategies = []) {
-    if (strategies.some((strategy) => !isStrategyOk(strategy))) {
-      throw new Error(`AirbagsApi expected array of strategies, got ${strategies}`);
-    }
-
-    if (!isContextOk(context)) {
-      throw new Error(`AirbagsApi expected context, got ${context}`);
-    }
-
-    this.strategies = strategies;
-    this.context = context;
+export default function createApi(context, strategies = []) {
+  if (strategies.some((strategy) => !isStrategyOk(strategy))) {
+    throw new Error(`createApi expected array of strategies, got ${strategies}`);
   }
 
-  /**
-   * Fetch the HTML for a page, from its' `nakedPath`
-   */
-  getPageHtml(nakedPath) {
-    /* eslint no-console:0 */
-    console.warn(`api.getPageHtml is deprecated, please use getPageData instead`);
-    return this._applyToStrategies('getPageHtml', this.context, nakedPath);
+  if (!isContextOk(context)) {
+    throw new Error(`createApi expected context, got ${context}`);
   }
 
-  /**
-   * Fetch all data for a page, from its' `nakedPath`
-   */
-  getPageData(nakedPath) {
-    return this._applyToStrategies('getPageData', this.context, nakedPath);
-  }
-
-  /**
-   * Run a method on strategies in order until one of them resolves. Reject if
-   * no strategies resolve.
-   */
-  _applyToStrategies(methodName, ...args) {
-    return new Promise((resolve, reject) => {
-      if (this.strategies.length === 0) {
-        return reject(new Error(`No strategies to use for ${methodName}`));
-      }
-
-      const apply = (iterator) => {
-        if (iterator === this.strategies.length) {
-          return reject(`No strategies could resolve ${methodName}`);
-        }
-
-        this.strategies[iterator][methodName](...args)
-          .then((data) => {
-            resolve(data);
-          })
-          .catch(() => {
-            apply(iterator + 1);
-          });
-      };
-
-      apply(0);
-    });
-  }
+  const api = {};
+  apiModel.call(api, context, strategies);
+  return api;
 }
